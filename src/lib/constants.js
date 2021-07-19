c7e9d8d883f3ece2dec67870a56b508fb009784a
Joseph Yu <niceyoyo27@gmail.com>
2021-07-19 21:34:53 +0800

    Updates

--- src/lib/constants.js
+++
@@ -1,6 +0,0 @@
-export const modalTypes = {
-  ADD: 'ADD',
-  EDIT: 'EDIT',
-};
-
-export default null;
--- src/lib/mutationTypes.js
+++
@@ -1,8 +0,0 @@
-const types = {
-  ADD_ITEM: 'ADD_ITEM',
-  EDIT_ITEM: 'EDIT_ITEM',
-  DELETE_ITEM: 'DELETE_ITEM',
-  SORT_ITEM_LIST: 'SORT_ITEM_LIST',
-};
-
-export default types;
